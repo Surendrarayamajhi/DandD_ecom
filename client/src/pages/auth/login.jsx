df331a364d6344a25d558916acd799ad6ef373ca
Surendrarayamajhi <suren.rayamajhi1425@gmail.com>
2026-01-25 01:59:19 +0545

    login page ui completed

--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import CommonForm from '@/components/common/form'
+import { loginFormControls } from '@/config'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+
+const initialState = {
+  userName : '',
+  email : '',
+  password : '',
+}
 
 const AuthLogin = () => {
+
+  const [formData, setFormData] = useState(initialState)
+
+  function onSubmit(){
+
+  }
   return (
-    <div>Login page</div>
+    <div className='mx-auto w-full max-w-md space-x-6'>
+      <div className='text-center'>
+        <h1 className='text-3xl font-bold tracking-tight text-foreground'> Create new account</h1>
+        <p className='mt-2'>Already Don't have an account?
+          <Link className='font-medium ml-2 text-primary hover:underline' to='/auth/register'>Register</Link>
+        </p>
+      </div>
+      <CommonForm 
+      formControls={loginFormControls}
+      buttonText={'Sign In'}
+      formData={formData}
+      setFormData={setFormData}
+      onSubmit={onSubmit}
+      />
+    </div>
   )
 }
 
