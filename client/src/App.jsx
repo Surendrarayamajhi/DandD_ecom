0fa9d4cc4672aa37e49fba1d7d68b6b3392be5a9
Surendrarayamajhi <suren.rayamajhi1425@gmail.com>
2026-01-28 02:30:29 +0545

    login or logout between refresh user get able to access depend cookie help of middleware created successfully

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,9 @@ import ShoppingCheckout from './pages/shopping-view/checkout'
 import ShoppingAccount from './pages/shopping-view/account'
 import CheckAuth from './components/common/check-auth'
 import UnauthPage from './pages/unauth-page'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { checkAuth } from './store/auth-slice'
 
 function App() {
 
@@ -25,7 +27,11 @@ function App() {
   // const user = null
 
   const {isAuthenticated, user} = useSelector(state => state.auth)
+  const dispatch = useDispatch();
 
+  useEffect(() =>{
+    dispatch(checkAuth());
+  }, [dispatch])
   return (
     <div className='flex flex-col overflow-hidden bg-white'>
       
